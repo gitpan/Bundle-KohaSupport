26701ce6eb4221e60b65865e7e9a02f037b70e40
Mike Mylonas <koha@dragon-is.co.nz>
2002-09-30 05:07:37 +0000

    Import of KOHA/Bundle-KohaSupport-0.02 from CPAN.
    gitpan-cpan-distribution: Bundle-KohaSupport
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KOHA/Bundle-KohaSupport-0.02.tar.gz
    gitpan-cpan-author:       KOHA
    gitpan-cpan-maturity:     released

--- KohaSupport.pm
+++ KohaSupport.pm
@@ -1,8 +1,6 @@
 package Bundle::KohaSupport;
 
-$VERSION = '0.01';
-
-1;
+$VERSION = '0.02';
 
 __END__
 
@@ -11,42 +9,35 @@ __END__
 Bundle::KohaSupport - A bundle of the modules required by Koha. 
 
 =head1 SYNOPSIS
+
 C<perl -MCPAN -e 'install Bundle::KohaSupport'>
 
 =head1 CONTENTS
 
-Date::Manip      - Date manipulation routines
-DBD::mysql       - mysql drivers for the Perl Database Interface
-DBI              - Database independent interface for Perl
-Digest::MD5      - Interface to the MD5 Algorithm
-HTML::Template   - Perl module to use HTML Templates from CGI scripts
-Set::Scalar      - Basic set operations
+Test::Harness		- Run perl standard test scripts with statistics 
+
+Test::More		- A framework for test scripts 
+
+Date::Manip		- Date manipulation routines
+
+DBD::mysql		- mysql drivers for the Perl Database Interface
+
+DBI			- Database independent interface for Perl
+
+Digest::MD5		- Interface to the MD5 Algorithm
+
+HTML::Template		- Perl module to use HTML Templates from CGI scripts
+
+MARC::Record		- Module for handling MARC records as objects
+
+Set::Scalar		- Basic set operations
 
 =head1 DESCRIPTION
 
-This bundle installs the prerequisites for Koha.
-
-=over 4
-
-=item Date::Manip
-Routines to manipulate date and time.
-
-=item DBD::mysql
-This module is used to connect to the database that Koha 
-uses to store its information.
-
-=item DBI
-A Database interface.
-
-=item Digest::MD5
-Interface to the MD5 Algorithm
-
-=item HTML::Template
-For templates in the Koha web based interface.
-
-=item Set:Scalar
-Basic Set operations.
+This bundle gathers together all installs all of the prerequisite 
+modules for Koha, the open source integrated library system.
 
 =head1 AUTHOR
 
 Mike Mylonas, <koha@dragon-is.co.nz>
+(Author only of this bundle, not necessarily of the modules that it lists)
